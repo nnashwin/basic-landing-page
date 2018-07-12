daf2efbcd9aa4b0ac0ca18a4915aa5fc69a527c9
Tyler Boright <lupin3.ken@gmail.com>
2018-07-11 22:33:01 -0700

    adds a new Section for the icons and adds some styled components to compose it

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import { StyledNav } from './components/Navbar';
 import { StyledHeader } from './components/Overlay';
+import { StyledIconSection } from './components/Section';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
 		<StyledNav links={[{href: "google.com", text: "Home"}, {href: "amazon.com", text: 'Sign Up', isButton: true}]}></StyledNav>
 		<StyledHeader imageUrl={"https://placekitten.com/1200/945"}></StyledHeader>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+		<StyledIconSection>
+			<p className="App-intro">
+			  To get started, edit <code>src/App.js</code> and save to reload.
+			</p>
+		</StyledIconSection>
       </div>
     );
   }
---
+++ src/components/Section.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const IconHolder = styled.div`
+	display: flex;
+	justify-content: space-around;
+	margin: 0 auto;
+	flex-wrap: wrap;
+	max-width: 1140px;
+`;
+
+const IconTitle = styled.h3`
+	font-size: 28px;
+	text-align: center;
+	font-weight: 700;
+	line-height: 12px;
+`;
+
+const IconDesc = styled.p`
+	font-size: 20px;
+	font-weight: 300;
+	line-height: 30px;
+	text-align: center;
+`;
+
+const IconDiv = styled.div`
+	max-width: 320px;
+`;
+
+const IconPicDiv = styled.div`
+	text-align: center;
+`;
+
+const IconSection = ({className}) => {
+	return (
+		<section className={className}>
+			<IconHolder>
+				<IconDiv>
+					<IconPicDiv>picture</IconPicDiv>
+					<IconTitle>Title</IconTitle>
+					<IconDesc>These are words which will go in the first paragraph</IconDesc>
+				</IconDiv>
+				<IconDiv>
+					<IconPicDiv>picture</IconPicDiv>
+					<IconTitle>Title</IconTitle>
+					<IconDesc>Use these words from the second paragrapn wisely</IconDesc>
+				</IconDiv>
+				<IconDiv>
+					<IconPicDiv>picture</IconPicDiv>
+					<IconTitle>Title</IconTitle>
+					<IconDesc>I love cake from the depths of my belly</IconDesc>
+				</IconDiv>
+			</IconHolder>
+		</section>
+	);
+}
+
+export const StyledIconSection = styled(IconSection)`
+	padding: 7rem 0;
+`;
