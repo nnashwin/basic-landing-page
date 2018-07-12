0ae5db7a3a2f86bfe5323ad659f9e980df31857c
Tyler Boright <lupin3.ken@gmail.com>
2018-07-11 22:33:19 -0700

    abstracts the Container component into the Shared file

---
+++ src/components/Shared.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+	width: 100%;
+	height: 100%;
+	max-width: 900px;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	box-sizing: border-box;
+	text-align: left;
+`;
